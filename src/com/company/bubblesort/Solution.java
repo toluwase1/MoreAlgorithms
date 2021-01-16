dea2755b4a18e2771d71cfc0648ef804086897dd
toluwase <toluwasethomas1@gmail.com>
2021-01-16 20:07:08 +0100

    Algorithms continued

--- src/com/company/bubblesort/Solution.java
+++ src/com/company/bubblesort/Solution.java
@@ -3,6 +3,7 @@ package com.company.bubblesort;
 public class Solution {
 
     public int solution(int number) {
+        //sum up the factors of number divisible by 3 and 5 less than or equal to number
         //TODO: Code stuff here
         int sum = 0;
         for (int i = 3; i<number; i++)
